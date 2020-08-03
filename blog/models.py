f65550af402fb953e1d4b499b35be41a11cc5cbc
tyan <taeyeon0107@naver.com>
2020-08-03 21:47:20 +0900

    1. models.py, urls.py, update.html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    image = models.ImageField(null=True, default='', upload_to="blogimg")
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
